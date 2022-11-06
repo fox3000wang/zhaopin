14ee184aeca14be91e7dc0462d5b291e631da132
fox.wang <fox3000wang@hotmail.com>
2022-11-06 17:46:20 +0800

    框架搭建完毕，等着写业务

--- server/src/report/report.service.ts
+++ server/src/report/report.service.ts
@@ -1,17 +1,40 @@
-import fs =  require('fs');
-import path =  require('path');
-import { Injectable } from '@nestjs/common';
-import { toDate, toInt } from 'src/utils';
-const XlsxTemplate = require('xlsx-template');
 
+import { Injectable } from '@nestjs/common';
+import {} from 'mysql2';
 @Injectable()
 export class ReportService {
+
+  connection:any = null;  
+
+  constructor(){
+    const mysql = require('mysql2');
+    
+    this.connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'user',
+      password:'user',
+      database: 'zhaopin',
+      charset:'utf8',
+    });
+    
+  }
 
   postRecord(data:any): any {
     if(!data) return 'data is null';
     console.log(`[postRecord]`, JSON.stringify(data));
     //console.log(`[postRecord] config`,JSON.stringify(data.config));
+    const {txt} = data;
 
+    console.log(`INSERT INTO jobs (title) VALUES (${txt})`);
+    this.connection.query(
+      // 'SELECT * FROM jobs; ',
+      `INSERT INTO jobs (title) VALUES ("${txt}")`,
+      function(err, results, fields) {
+        console.log(results); // results contains rows returned by server
+        console.log(fields); // fields contains extra meta data about results, if available
+      }
+    );
+    
   }
 
 }
