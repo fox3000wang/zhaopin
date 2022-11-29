7255a596d287e2ff39112d29449b0fc4eb5dada9
fox.wang <fox3000wang@hotmail.com>
2022-11-29 10:11:01 +0800

    打包脚本完成

--- server/src/base/base.service.ts
+++ server/src/base/base.service.ts
@@ -1,15 +1,19 @@
 export class BaseService {
   pool: any;
   constructor() {
+    console.log('process.env.RUNNING_ENV', process.env.RUNNING_ENV);
+    const IS_PROD = process.env.RUNNING_ENV === 'prod';
+
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     const mysql = require('mysql2');
     this.pool = mysql.createPool({
-      host: process.env.HOST,
+      host: IS_PROD ? 'wangzm.cn' : '192.168.0.104', // TODO: add to config
       user: 'user',
       password: 'useruser',
       database: 'zhaopin',
       charset: 'utf8',
     });
+    console.log(`[process.env.HOST]`, IS_PROD ? 'wangzm.cn' : '192.168.0.104');
   }
 
   // 封装成可以同步执行的函数
